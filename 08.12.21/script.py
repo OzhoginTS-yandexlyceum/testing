1708559f87d93acbaeb61dc80b9dececfddc2ee7
Ozhogin Tikhon <tot_2005@bk.ru>
2021-12-08 17:07:59 +0300

    superfunction - "What an awesome print!"

---
+++ 08.12.21/script.py
@@ -0,0 +1,10 @@
+def my_superfunction():
+    print('What an awesome print!')
+
+
+if __name__ == '__main__':
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
